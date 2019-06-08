1ed87b2807bbff9500c89d8425cf57f1435ac5a5
Brendan <2bndy5@gmail.com>
2019-06-07 22:34:28 -0700

    implement fake celleration on dc motors NO DELAY

--- Motor_N_Sensors_node/Motor.cpp
+++ Motor_N_Sensors_node/Motor.cpp
@@ -1,39 +1,80 @@
 #include "Motor.h"
 #include <arduino.h>
 
-// Function definitions for BiMotor class
-BiMotor::BiMotor(unsigned char pin1, unsigned char pin2): Motor(pin1, pin2){
+
+Motor::Motor(unsigned char pin1, unsigned char pin2, int dt){
+    pinMode(pin1, OUTPUT);
+    pinMode(pin2, OUTPUT);
+    rampTime = dt;
+    isUp= false;
     // save pin #s and set pin mode
-    pOut1 = pin1;
-    pOut2 = pin2;
+    _pin1 = pin1;
+    _pin2 = pin2;
+    signal1 = 0;
+    signal2 = 0;
 }
-void BiMotor::go(short s){
+
+short Motor::fakeCellerate(long now){
+    long timeI = now - initSmooth;
+    float delta_speed = sin( timeI / rampTime * PI / 2 + (-1 * isUp * PI / 2) ) + isUp;
+    write((short)(delta_speed * (targetSpeed - baseSpeed) + baseSpeed));
+}
+
+void Motor::go(short s){
+    targetSpeed = max(-100, min(100, round(s * 100)));
+    initSmooth = millis();
+    baseSpeed = getSpeed(); 
+    float deltaT = abs((targetSpeed - baseSpeed) / 100.0);
+    endSmooth = initSmooth + deltaT * rampTime;
+    isUp = targetSpeed > baseSpeed;
+    fakeCellerate(initSmooth + 1);
+}
+
+void Motor::tick(){
+    short currTime = millis();
+    if (abs(getSpeed() - targetSpeed) >= 2  && currTime < endSmooth){
+        fakeCellerate(currTime);
+    }
+}
+
+// Function definitions for BiMotor class
+BiMotor::BiMotor(unsigned char pin1, unsigned char pin2, int dt): Motor(pin1, pin2, dt){}
+
+void BiMotor::write(short s){
     //input speed 's' must be previously constrained to range of [-255, 255]
     if (s < 0){ // if going backward
-        analogWrite(pOut1, 0);
-        analogWrite(pOut2, -1 * s);
+        signal1 = 0;
+        signal2 = -1 * s;
     }
     else if (s >= 0){// if going forward or stopped
-        analogWrite(pOut1, s);
-        analogWrite(pOut2, 0);
+        signal1 = s;
+        signal2 = 0;
     }
+    analogWrite(_pin1, signal1);
+    analogWrite(_pin2, signal2);
     // Serial.print("motor pwm: ");Serial.println(s);
 }
-// Function definitions for PhasedMotor class
-PhasedMotor::PhasedMotor(unsigned char pin1, unsigned char pin2): Motor(pin1, pin2){
-    // save pin #s and set pin mode
-    dirPin = pin1;
-    pwmPin = pin2;
+
+short BiMotor::getSpeed(){
+    return (short)signal1 - signal2;
 }
-void PhasedMotor::go(short s){
+
+// Function definitions for PhasedMotor class
+PhasedMotor::PhasedMotor(unsigned char pin1, unsigned char pin2, int dt): Motor(pin1, pin2, dt){
+}
+void PhasedMotor::write(short s){
     //input speed 's' must be previously constrained to range of [-255, 255]
     if (s <= 0){     // if going backward or stopped
-        digitalWrite(dirPin, false);
-        analogWrite(pwmPin, -1 * s);
+        signal1 = false;
+        signal2 = -1 * s;
     }
     else if (s > 0){// if going forward
-        digitalWrite(dirPin, true);
-        analogWrite(pwmPin, s);
+        signal1 = true;
+        signal2 = s;
     }
-    // Serial.print("motor pwm: ");Serial.println(s);
+    digitalWrite(_pin1, signal1);
+    analogWrite(_pin2, signal2);
+}
+short PhasedMotor::getSpeed(){ 
+    return (short)signal2 - (!signal1 * 255);
 }
--- Motor_N_Sensors_node/Motor.h
+++ Motor_N_Sensors_node/Motor.h
@@ -3,30 +3,34 @@
 #include <arduino.h>
 
 class Motor{
+    bool isUp;
+    int rampTime;
+    short targetSpeed, baseSpeed;
+    long initSmooth, endSmooth;
+    short fakeCellerate(long now);
 public:
-    Motor(unsigned char pin1, unsigned char pin2){
-        pinMode(pin1, OUTPUT);
-        pinMode(pin2, OUTPUT);
-    }
-    virtual void go(short){};
+    Motor(unsigned char pin1, unsigned char pin2, int dt = 1000);
+    virtual short getSpeed(){};
+    virtual void write(short){};
+    void go(short);
+    void tick();
+protected:
+    unsigned char _pin1, _pin2, signal1, signal2;
 };
 
 class BiMotor: public Motor{
-    // class for instantiating DC drive motor(s)
-    // variables for pin #s
-    unsigned char pOut1, pOut2;
+    
 public:
-    BiMotor(unsigned char, unsigned char);// c'tor
-    void go(short);// speed can be in range [-255, 255]
+    BiMotor(unsigned char, unsigned char, int dt = 1000);
+    void write(short);
+    short getSpeed();
 };
 
 class PhasedMotor: public Motor{
-    // class for instantiating DC drive motor(s)
-    // variables for pin #s
-    unsigned char dirPin, pwmPin;
 public:
-    PhasedMotor(unsigned char, unsigned char);// c'tor
-    void go(short);// speed can be in range [-255, 255]
+    PhasedMotor(unsigned char, unsigned char, int dt = 1000);
+    void write(short);
+    short getSpeed();
 };
 
 #endif
